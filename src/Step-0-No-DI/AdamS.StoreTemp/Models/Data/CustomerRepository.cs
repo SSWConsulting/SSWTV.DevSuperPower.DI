9dcc8f0f572cec040071f27a06224bab8bdfc39f
Adam Stephensen <mail@adamstephensen.com>
2015-02-19 13:18:50 +1000

    Code Tidy of Repositories

--- src/Step-0-No-DI/AdamS.StoreTemp/Models/Data/CustomerRepository.cs
+++ src/Step-0-No-DI/AdamS.StoreTemp/Models/Data/CustomerRepository.cs
@@ -3,18 +3,18 @@ using System.Linq;
 
 namespace AdamS.OnlineStore.Models
 {
-    
-    public class CustomerRepository 
+    public class CustomerRepository
     {
-        private List<Customer> _customers = new List<Customer>
-            {
-                new Customer{Id=1,FirstName = "Adam", LastName = "Apple"}, 
-                new Customer{Id=2,FirstName = "Brad", LastName = "Banana"},
-                new Customer{Id=3,FirstName = "Craig", LastName = "Canary"}
-            };
-        public Customer Get(int id)
+        private List<Customer> _customers;
+
+        public CustomerRepository()
         {
-            return _customers.Single(cust => cust.Id == id);
+            _customers = new List<Customer>
+            {
+                new Customer {Id = 1, FirstName = "Adam", LastName = "Apple"},
+                new Customer {Id = 2, FirstName = "Brad", LastName = "Banana"},
+                new Customer {Id = 3, FirstName = "Craig", LastName = "Canary"}
+            };
         }
 
         public List<Customer> Get()
@@ -22,14 +22,18 @@ namespace AdamS.OnlineStore.Models
             return _customers;
         }
 
+        public Customer Get(int id)
+        {
+            return _customers.Single(cust => cust.Id == id);
+        }
+        
         public Customer Add(Customer customer)
         {
             _customers.Add(customer);
             //todo: add Id field
             return customer;
         }
-
-
+        
         public Customer Delete(int id)
         {
             Customer itemToRemove = _customers.SingleOrDefault(c => c.Id == id);
@@ -39,9 +43,6 @@ namespace AdamS.OnlineStore.Models
                 _customers.Remove(itemToRemove);
             }
             return itemToRemove;
-
-
         }
     }
-
 }
--- src/Step-0-No-DI/AdamS.StoreTemp/Models/Data/OrderRepository.cs
+++ src/Step-0-No-DI/AdamS.StoreTemp/Models/Data/OrderRepository.cs
@@ -5,15 +5,17 @@ namespace AdamS.OnlineStore.Models
 {
     public class OrderRepository
     {
+        private List<Order> _orders;
 
-        private List<Order> _orders = new List<Order>
+        public OrderRepository()
         {
-            new Order{Id=1,CustomerId = 1, OrderTotal = 12.45M}, 
-            new Order{Id=2,CustomerId = 1, OrderTotal = 23.00M}, 
-            new Order{Id=3,CustomerId = 1, OrderTotal = 54.50M}, 
-        };
+            _orders = new List<Order>{
+                new Order{Id=1,CustomerId = 1, OrderTotal = 12.45M}, 
+                new Order{Id=2,CustomerId = 1, OrderTotal = 23.00M}, 
+                new Order{Id=3,CustomerId = 1, OrderTotal = 54.50M}, 
+            };
+        }
 
-        
         public List<Order> Get(int customerId)
         {
             return _orders.Where(c => c.CustomerId == customerId).ToList();
