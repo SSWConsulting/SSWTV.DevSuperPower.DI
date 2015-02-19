655a63bd61dca22f94055156c5ca10519f65d197
Adam Stephensen <mail@adamstephensen.com>
2015-02-19 13:13:39 +1000

    Update StringExtension to validate email

--- src/Step-0-No-DI/AdamS.StoreTemp/Models/Common/StringExtensions.cs
+++ src/Step-0-No-DI/AdamS.StoreTemp/Models/Common/StringExtensions.cs
@@ -1,10 +1,17 @@
+using System.Text.RegularExpressions;
+
 namespace AdamS.StoreTemp.Models.Common
 {
     public static class StringExtensions
     {
         public static bool IsValidEmailAddress(this string emailAddress)
         {
-            return !string.IsNullOrWhiteSpace(emailAddress);
+            Regex regex = new Regex(@"^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$");
+            Match match = regex.Match(emailAddress);
+            return match.Success;
+
         }
+        
     }
+    
 }
